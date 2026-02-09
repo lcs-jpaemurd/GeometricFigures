d0b497722f5ab6c13e32879922d6e1141197e39c
Joshua Paemurd <jpaemurd@lcs.on.ca>
2026-02-09 14:24:53 -0500

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Joshua Paemurd on 2026-02-09.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
